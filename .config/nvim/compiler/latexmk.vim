ca1062c199d63d55f13100eff8d2d0f441173d3d
phelipe <phelipe_teles@hotmail.com>
2020-10-11 13:47:02 -0300

    Update tex ftplugin

--- .config/nvim/compiler/latexmk.vim
+++ .config/nvim/compiler/latexmk.vim
@@ -7,5 +7,5 @@ if exists(":CompilerSet") != 2		" older Vim always used :setlocal
   command -nargs=* CompilerSet setlocal <args>
 endif
 
-CompilerSet makeprg=latexmk\ -pv
+CompilerSet makeprg=latexmk
 CompilerSet errorformat+=%f:%l:\ %m,%-G%.%#
---
+++ .config/nvim/lua/latexmk.lua
@@ -0,0 +1,37 @@
+local M = {}
+
+local results = {}
+
+function M.run()
+  local winnr = vim.fn.win_getid()
+  local bufname = vim.fn.expand("%")
+
+  vim.api.nvim_command("compiler latexmk")
+
+  local lines = {}
+
+  local function on_event(job_id, data, event)
+    if event == "stdout" or event == "stderr" then
+      vim.fn.setloclist(winnr, {}, "a", {
+          title = "Latexmk",
+          lines = data,
+          efm = vim.api.nvim_buf_get_option(bufnr, "errorformat")
+        }
+      )
+    end
+  end
+
+  local pid =
+    vim.fn.jobstart(
+    string.format("latexmk -pvc %s", bufname),
+    {
+      on_stdout = on_event,
+      on_stderr = on_event,
+      on_exit = on_event
+    }
+  )
+
+  print("Latexmk is running. PID: "..pid)
+end
+
+return M
