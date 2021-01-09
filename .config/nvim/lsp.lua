9472017d2ca2aa0b4cf309b6144f7e59b8cee255
Phelipe Teles <phelipe_teles@hotmail.com>
2021-01-08 22:12:58 -0300

    Refactoring lsp config

--- .config/nvim/lsp.lua
+++ .config/nvim/lsp.lua
@@ -1,7 +1,6 @@
 vim.lsp.stop_client(vim.lsp.buf_get_clients())
 
 local nvim_lsp = require "nvim_lsp"
-local util = require "nvim_lsp/util"
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] =
   vim.lsp.with(
@@ -12,32 +11,59 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] =
   }
 )
 
-local function set_lsp_config(_)
-  vim.api.nvim_command [[setlocal omnifunc=v:lua.vim.lsp.omnifunc]]
+local function set_lsp_config(client)
   vim.api.nvim_command [[setlocal signcolumn=yes]]
-  vim.api.nvim_command [[nnoremap <buffer><silent> K :lua vim.lsp.buf.hover()<CR>]]
-  vim.api.nvim_command [[nnoremap <buffer><silent> gd :lua vim.lsp.buf.definition()<CR>]]
-  vim.api.nvim_command [[nnoremap <buffer><silent> [d :lua vim.lsp.buf.definition()<CR>]]
-  vim.api.nvim_command [[nnoremap <buffer><silent> [<C-d> :lua vim.lsp.buf.definition()<CR>]]
-  vim.api.nvim_command [[nnoremap <buffer><silent> <C-w><C-d> :split <bar> lua vim.lsp.buf.definition()<CR>]]
-  vim.api.nvim_command [[nnoremap <buffer><silent> [t :lua vim.lsp.buf.type_definition()<CR>]]
-  vim.api.nvim_command [[nnoremap <buffer><silent> [<C-t> :lua vim.lsp.buf.type_definition()<CR>]]
-  vim.api.nvim_command [[nnoremap <buffer><silent> <C-w><C-t> :lua vim.lsp.buf.type_definition()<CR>]]
-  vim.api.nvim_command [[nnoremap <buffer><silent> gr :lua vim.lsp.buf.references()<CR>]]
-  vim.api.nvim_command [[nnoremap <buffer><silent> gs :lua vim.lsp.buf.workspace_symbol()<CR>]]
-  vim.api.nvim_command [[nnoremap <buffer><silent> gR :lua vim.lsp.buf.rename()<CR>]]
-  vim.api.nvim_command [[nnoremap <buffer><silent> <M-CR> :lua vim.lsp.buf.code_action()<CR>]]
-  vim.api.nvim_command [[vnoremap <buffer><silent> <M-CR> :lua vim.lsp.buf.range_code_action()<CR>]]
+  vim.api.nvim_command [[setlocal omnifunc=v:lua.vim.lsp.omnifunc]]
+
   vim.api.nvim_command [[nnoremap <buffer><silent> <C-space> :lua vim.lsp.diagnostic.show_line_diagnostics()<CR>]]
   vim.api.nvim_command [[nnoremap <buffer><silent> ]g :lua vim.lsp.diagnostic.goto_next()<CR>]]
   vim.api.nvim_command [[nnoremap <buffer><silent> [g :lua vim.lsp.diagnostic.goto_prev()<CR>]]
   vim.api.nvim_command [[nnoremap <buffer><silent> <space>d :lua vim.lsp.diagnostic.set_loclist()<CR>]]
-  vim.api.nvim_command [[command! -buffer Fmt lua vim.lsp.buf.formatting_sync(nil, 1000)]]
-  vim.api.nvim_command [[autocmd! BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 100)]]
+
+  if client.resolved_capabilities.hover then
+    vim.api.nvim_command [[nnoremap <buffer><silent> K :lua vim.lsp.buf.hover()<CR>]]
+  end
+
+  if client.resolved_capabilities.goto_definition then
+    vim.api.nvim_command [[nnoremap <buffer><silent> gd :lua vim.lsp.buf.definition()<CR>]]
+    vim.api.nvim_command [[nnoremap <buffer><silent> [d :lua vim.lsp.buf.definition()<CR>]]
+    vim.api.nvim_command [[nnoremap <buffer><silent> [<C-d> :lua vim.lsp.buf.definition()<CR>]]
+    vim.api.nvim_command [[nnoremap <buffer><silent> <C-w><C-d> :vsplit <bar> lua vim.lsp.buf.definition()<CR>]]
+  end
+
+  if client.resolved_capabilities.type_definition then
+    vim.api.nvim_command [[nnoremap <buffer><silent> [t :lua vim.lsp.buf.type_definition()<CR>]]
+    vim.api.nvim_command [[nnoremap <buffer><silent> [<C-t> :lua vim.lsp.buf.type_definition()<CR>]]
+    vim.api.nvim_command [[nnoremap <buffer><silent> <C-w><C-t> :lua vim.lsp.buf.type_definition()<CR>]]
+  end
+
+  if client.resolved_capabilities.find_references then
+    vim.api.nvim_command [[nnoremap <buffer><silent> gr :lua vim.lsp.buf.references()<CR>]]
+  end
+
+  if client.resolved_capabilities.rename then
+    vim.api.nvim_command [[nnoremap <buffer><silent> gR :lua vim.lsp.buf.rename()<CR>]]
+  end
+
+  if client.resolved_capabilities.workspace_symbol then
+    vim.api.nvim_command [[nnoremap <buffer><silent> gs :lua vim.lsp.buf.workspace_symbol()<CR>]]
+  end
+
+  if client.resolved_capabilities.client_action then
+    vim.api.nvim_command [[nnoremap <buffer><silent> <M-CR> :lua vim.lsp.buf.code_action()<CR>]]
+    vim.api.nvim_command [[vnoremap <buffer><silent> <M-CR> :lua vim.lsp.buf.range_code_action()<CR>]]
+  end
+
+  if client.resolved_capabilities.document_formatting then
+    vim.api.nvim_command [[command! -buffer Fmt lua vim.lsp.buf.formatting_sync(nil, 1000)]]
+    vim.api.nvim_command [[autocmd! BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 1000)]]
+  end
 end
 
 nvim_lsp.pyls.setup {
-  on_attach = set_lsp_config,
+  on_attach = function(client)
+    set_lsp_config(client)
+  end,
   settings = {
     pyls = {
       configurationSources = {"flake8"},
@@ -52,25 +78,12 @@ nvim_lsp.pyls.setup {
 
 nvim_lsp.tsserver.setup {
   on_attach = function(client)
-    set_lsp_config()
+    set_lsp_config(client)
     client.resolved_capabilities.document_formatting = false
   end
 }
 
-local efm_settings = {
-  languages = {}
-}
-
-local js_filetypes = {
-  "javascript",
-  "javascriptreact",
-  "javascript.jsx",
-  "typescript",
-  "typescript.tsx",
-  "typescriptreact"
-}
-
-local eslint_config = {
+local eslint = {
   lintCommand = "eslint_d -f unix --stdin --stdin-filename ${INPUT}",
   lintStdin = true,
   lintFormats = {"%f:%l:%c: %m"},
@@ -79,20 +92,9 @@ local eslint_config = {
   formatStdin = true
 }
 
-local prettier_config = {
-  formatCommand = "prettier --parser typescript",
-  formatStdin = true
-}
-
-local js_config = vim.fn.glob(".prettierrc*") == "" and {eslint_config} or {eslint_config, prettier_config}
-
-for _, ft in ipairs(js_filetypes) do
-  efm_settings.languages[ft] = js_config
-end
-
 nvim_lsp.efm.setup {
   on_attach = function(client)
-    set_lsp_config()
+    set_lsp_config(client)
     client.resolved_capabilities.document_formatting = true
   end,
   default_config = {
@@ -105,6 +107,22 @@ nvim_lsp.efm.setup {
   root_dir = function()
     return vim.fn.getcwd()
   end,
-  settings = efm_settings,
-  filetypes = js_filetypes
+  settings = {
+    languages = {
+      javascript = {eslint},
+      javascriptreact = {eslint},
+      ["javascript.jsx"] = {eslint},
+      typescript = {eslint},
+      ["typescript.tsx"] = {eslint},
+      typescriptreact = {eslint}
+    }
+  },
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "javascript.jsx",
+    "typescript",
+    "typescript.tsx",
+    "typescriptreact"
+  }
 }
