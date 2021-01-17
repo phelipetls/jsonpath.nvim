7c9cdce8e5fde0a1e797cdb826032f6632b204c5
Phelipe Teles <phelipe_teles@hotmail.com>
2021-01-17 07:08:31 -0300

    Customize rc.lua

--- .config/awesome/theme.lua
+++ .config/awesome/theme.lua
@@ -11,7 +11,7 @@ local themes_path = gfs.get_themes_dir()
 
 local theme = {}
 
-theme.font          = "sans 8"
+theme.font          = "Source Code Pro 9"
 
 theme.bg_normal     = "#222222"
 theme.bg_focus      = "#535d6c"
