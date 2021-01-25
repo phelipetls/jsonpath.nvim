88a7aa4aeb1ca9ade0b61fca57870b57351aff02
Phelipe Teles <phelipe_teles@hotmail.com>
2021-01-24 21:54:11 -0300

    Reduce border size in dwm

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -3,7 +3,7 @@
 
 /* appearance */
 static const unsigned int gappx     = 0;        /* gap pixel between windows */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
