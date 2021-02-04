cafd3fb92d20d20001efad8262e5900360af54da
Phelipe Teles <phelipe_teles@hotmail.com>
2021-02-04 19:36:44 -0300

    Show virtual_text in nvim LSP

--- .config/nvim/lsp.lua
+++ .config/nvim/lsp.lua
@@ -6,8 +6,8 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] =
   vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics,
   {
-    virtual_text = false,
-    underline = false
+    underline = false,
+    update_in_insert = false
   }
 )
 
