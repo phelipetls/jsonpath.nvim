a5ab9ecc2f37df5b8291cd853913f61f86eba4de
Phelipe Teles <phelipe_teles@hotmail.com>
2021-02-15 19:56:04 -0300

    Refactoring js_utils lua module

--- .config/nvim/lua/js_utils.lua
+++ .config/nvim/lua/js_utils.lua
@@ -1,48 +1,31 @@
 local M = {}
 
-function M.prettier_config_exists()
-  local prettierrc = vim.fn.glob(".prettierrc*", 0, 1)
-
-  if not vim.tbl_isempty(prettierrc) then
-    return true
-  end
-
+local function exists_package_json_field(field)
   if vim.fn.filereadable("package.json") ~= 0 then
-    if vim.fn.json_decode(vim.fn.readfile("package.json"))["prettier"] then
-      return true
-    end
+    local package_json = vim.fn.json_decode(vim.fn.readfile("package.json"))
+    return package_json[field] ~= nil
   end
+end
 
-  return false
+local function exists_glob(glob)
+  return vim.fn.glob(glob) ~= ""
+end
+
+function M.prettier_config_exists()
+  return exists_glob(".prettierrc*") or exists_package_json_field("prettier")
 end
 
 function M.eslint_config_exists()
-  local eslintrc = vim.fn.glob(".eslintrc*", 0, 1)
-
-  if not vim.tbl_isempty(eslintrc) then
-    return true
-  end
-
-  if vim.fn.filereadable("package.json") ~= 0 then
-    if vim.fn.json_decode(vim.fn.readfile("package.json"))["eslintConfig"] then
-      return true
-    end
-  end
-
-  return false
+  return exists_glob(".eslintrc*") or exists_package_json_field("eslintConfig")
 end
 
 function M.get_js_formatter()
-  if M.prettier_config_exists() then
-    if vim.fn.executable("prettier_d") then
-      return "prettier_d --parser=typescript"
-    end
+  if M.prettier_config_exists() and vim.fn.executable("prettier_d")  then
+    return "prettier_d --parser=typescript"
   end
 
-  if M.eslint_config_exists() then
-    if vim.fn.executable("eslint_d") then
-      return "eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}"
-    end
+  if M.eslint_config_exists() and vim.fn.executable("eslint_d")  then
+    return "eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}"
   end
 
   return ""
