24176c21e626d501143d5b63183abc528525e111
Phelipe Teles <phelipe_teles@hotmail.com>
2021-06-30 21:35:49 -0300

    Reduce font size of dwm status bar

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -11,7 +11,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "SourceCodePro-SemiBold:size=10" };
+static const char *fonts[]          = { "SourceCodePro-SemiBold:size=9" };
 static const char dmenufont[]       = "monospace:size=10";
 #include "/home/phelipe/.cache/wal/colors-wal-dwm.h"
 
