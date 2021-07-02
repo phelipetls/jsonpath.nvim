72913469fe5369912ae9bef36e0e806955b4661a
Phelipe Teles <phelipe_teles@hotmail.com>
2021-07-02 13:57:00 -0300

    Customize dmenu colors pywal template

---
+++ .config/wal/templates/colors-wal-dmenu.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {{
+	/*     fg         bg       */
+	[SchemeNorm] = {{ "{foreground}", "{background}" }},
+	[SchemeSel] = {{ "{foreground}", "{color8}" }},
+	[SchemeOut] = {{ "{foreground}", "{color5}" }},
+}};
