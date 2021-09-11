dc4e1d560288434c1d9c8f3c35f05e2f1a518b4c
Phelipe Teles <phelipe_teles@hotmail.com>
2021-09-11 16:46:01 -0300

    Remove unused Lua path module functions

--- .config/nvim/lua/path.lua
+++ .config/nvim/lua/path.lua
@@ -1,19 +1,11 @@
 local M = {}
 
-function M.get_current_file_dir()
-  return vim.fn.expand("%:p:h")
-end
-
 function M.path_exists(path)
   return vim.fn.filereadable(path) == 1 or vim.fn.isdirectory(path) == 1
 end
 
 function M.path_join(...)
   return vim.fn.simplify(table.concat({...}, "/"))
-end
-
-function M.remove_extension(fname)
-  return vim.fn.fnamemodify(fname, ":r")
 end
 
 return M
