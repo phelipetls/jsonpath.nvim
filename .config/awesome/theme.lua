ba1b6ea3617067c7a94dcb3ba66dd550e8e080e4
Phelipe Teles <phelipe_teles@hotmail.com>
2021-10-21 22:30:21 -0300

    Set icons in awesome theme

--- .config/awesome/theme.lua
+++ .config/awesome/theme.lua
@@ -125,7 +125,7 @@ theme.awesome_icon = theme_assets.awesome_icon(
 
 -- Define the icon theme for application icons. If not set then the icons
 -- from /usr/share/icons and /usr/share/icons/hicolor will be used.
-theme.icon_theme = nil
+theme.icon_theme = "/usr/share/icons/Papirus-Dark"
 
 return theme
 
