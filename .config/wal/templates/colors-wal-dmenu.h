e953a7c67a190eb6458cf8fd0b84a6c105990bb6
Phelipe Teles <phelipe_teles@hotmail.com>
2021-11-29 20:36:54 -0300

    More consistent dmenu color with dwm

--- .config/wal/templates/colors-wal-dmenu.h
+++ .config/wal/templates/colors-wal-dmenu.h
@@ -1,6 +1,6 @@
 static const char *colors[SchemeLast][2] = {{
 	/*     fg         bg       */
 	[SchemeNorm] = {{ "{foreground}", "{background}" }},
-	[SchemeSel] = {{ "{foreground}", "{color8}" }},
+	[SchemeSel] = {{ "{foreground}", "{color0}" }},
 	[SchemeOut] = {{ "{foreground}", "{color5}" }},
 }};
