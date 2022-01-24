29cbae600341c023d1c8b65e7a058b12b098188d
Phelipe Teles <phelipe_teles@hotmail.com>
2022-01-23 23:03:38 -0300

    Change default terminal from st to alacritty in dwm

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -83,7 +83,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2]                     = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]               = { "dmenu_run" };
-static const char *termcmd[]                = { "st", NULL };
+static const char *termcmd[]                = { "alacritty", NULL };
 
 #include "focusurgent.c"
 
