e878b434fbfd1a1e2138fc4a4f31964c1557fe6d
Phelipe Teles <phelipe_teles@hotmail.com>
2022-04-14 19:45:38 -0300

    Remove dwm and all software and config I used with it

--- .config/wal/templates/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {{
-	/*     fg         bg       */
-	[SchemeNorm] = {{ "{foreground}", "{background}" }},
-	[SchemeSel] = {{ "{color15}", "{color8}" }},
-	[SchemeOut] = {{ "{foreground}", "{color5}" }},
-}};
