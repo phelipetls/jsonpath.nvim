b6db8edbd8c37f99cb8095bc926cecd8505c0a2c
Phelipe Teles <phelipe_teles@hotmail.com>
2022-04-21 17:19:36 -0300

    Fix slock not working in Fedora
    See https://gitlab.com/-/snippets/2048501

--- suckless/slock/config.h
+++ suckless/slock/config.h
@@ -1,6 +1,6 @@
 /* user and group to drop privileges to */
 static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *group = "nobody";
 
 #include "colors-wal-slock.h"
 
