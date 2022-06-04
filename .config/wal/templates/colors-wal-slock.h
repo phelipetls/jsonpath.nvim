7606f5c6570be47225d845dc36639bb93cf17d1b
Phelipe Teles <phelipe_teles@hotmail.com>
2022-06-04 19:01:24 -0300

    Remove suckless desktop environment and related software

--- .config/wal/templates/colors-wal-slock.h
+++
@@ -1,5 +0,0 @@
-static const char *colorname[NUMCOLS] = {{
-	[INIT] =   "{background}",     /* after initialization */
-	[INPUT] =  "{color4}",   /* during input */
-	[FAILED] = "{color1}",   /* wrong password */
-}};
