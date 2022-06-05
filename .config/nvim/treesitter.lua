e837ef935f6aa2fbb1b15942c7443962c3a5b639
Phelipe Teles <phelipe_teles@hotmail.com>
2022-06-05 15:34:58 -0300

    Use treesitter to highlight YAML files

--- .config/nvim/treesitter.lua
+++ .config/nvim/treesitter.lua
@@ -2,7 +2,7 @@ vim.api.nvim_command([[packadd! nvim-treesitter]])
 vim.api.nvim_command([[packadd! nvim-ts-context-commentstring]])
 
 require("nvim-treesitter.configs").setup({
-  ensure_installed = { "typescript", "javascript", "tsx", "toml", "jsonc", "python" },
+  ensure_installed = { "typescript", "javascript", "tsx", "toml", "jsonc", "python", "yaml" },
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
