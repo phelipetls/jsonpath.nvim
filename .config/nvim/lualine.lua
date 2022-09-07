f6c4e2a2e06071c1f9adf61b41c450a980e2abaa
Phelipe Teles <phelipe_teles@hotmail.com>
2022-09-07 08:51:53 -0300

    Reorganize how to configure lualine

--- .config/nvim/lualine.lua
+++ .config/nvim/lualine.lua
@@ -11,29 +11,49 @@ local fugitivestatusline = function()
   return revision or branch or ""
 end
 
-local filename = function()
-  local fullpath = vim.fn.expand("%:p")
-  local fname = vim.fn.expand("%:t")
+local filename = {
+  function()
+    local fullpath = vim.fn.expand("%:p")
+    local fname = vim.fn.expand("%:t")
 
-  fname = fname ~= "" and fname or "[No Name]"
+    fname = fname ~= "" and fname or "[No Name]"
 
-  if fullpath:find("^fugitive://") then
-    local matches = vim.fn.matchlist(
-      fullpath,
-      [[\c^fugitive:\%(//\)\=\(.\{-\}\)\%(//\|::\)\(\x\{40,\}\|[0-3]\)\(/.*\)\=$]]
-    )
-    local revision = matches[3]
-    if revision and revision ~= "" then
-      local revisionlabel = revision == "0" and "index" or string.sub(revision, 0, 7)
-      fname = string.format("%s:%s", revisionlabel, fname)
+    if fullpath:find("^fugitive://") then
+      local matches = vim.fn.matchlist(
+        fullpath,
+        [[\c^fugitive:\%(//\)\=\(.\{-\}\)\%(//\|::\)\(\x\{40,\}\|[0-3]\)\(/.*\)\=$]]
+      )
+      local revision = matches[3]
+      if revision and revision ~= "" then
+        local revisionlabel = revision == "0" and "index" or string.sub(revision, 0, 7)
+        fname = string.format("%s:%s", revisionlabel, fname)
+      end
     end
-  end
 
-  local modified = vim.bo.modified and "[+]" or ""
-  local readonly = (not vim.bo.modifiable or vim.bo.readonly) and "[-]" or ""
+    return fname
+  end,
+  padding = { left = 1, right = 1 },
+}
 
-  return string.format("%s %s %s", fname, modified, readonly)
-end
+local modified = {
+  function()
+    return "[+]"
+  end,
+  cond = function()
+    return vim.bo.modified
+  end,
+  padding = { left = 0, right = 1 },
+}
+
+local readonly = {
+  function()
+    return "[-]"
+  end,
+  cond = function()
+    return not vim.bo.modifiable or vim.bo.readonly
+  end,
+  padding = { left = 0, right = 1 },
+}
 
 package.path = package.path .. ";" .. table.concat({ os.getenv("HOME"), ".cache", "wal", "?.lua" }, "/")
 local wal_colors = require("colors")
@@ -62,9 +82,9 @@ require("lualine").setup({
       "g:async_make_status",
     },
     lualine_c = {
-      {
-        filename,
-      },
+      filename,
+      modified,
+      readonly,
     },
     lualine_x = { "diagnostics", "fileformat", "filetype" },
     lualine_y = {},
@@ -78,7 +98,11 @@ require("lualine").setup({
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = { filename },
+    lualine_c = {
+      filename,
+      modified,
+      readonly,
+    },
     lualine_x = {},
     lualine_y = {},
     lualine_z = {
