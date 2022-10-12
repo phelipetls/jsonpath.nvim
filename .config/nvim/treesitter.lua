fea0130617836e9b43e483fa34615a7f5b48adde
Phelipe Teles <phelipe_teles@hotmail.com>
2022-10-12 12:59:32 -0300

    Remove nvim-treesitter-textobjects
    Never worked for me

--- .config/nvim/treesitter.lua
+++ .config/nvim/treesitter.lua
@@ -1,7 +1,6 @@
 vim.api.nvim_command([[packadd! nvim-treesitter]])
 vim.api.nvim_command([[packadd! nvim-ts-context-commentstring]])
 vim.api.nvim_command([[packadd! nvim-ts-autotag]])
-vim.api.nvim_command([[packadd! nvim-treesitter-textobjects]])
 vim.api.nvim_command([[packadd! playground]])
 
 require("nvim-treesitter.configs").setup({
@@ -48,62 +47,6 @@ require("nvim-treesitter.configs").setup({
       "tsx",
       "jsx",
       "markdown",
-    },
-  },
-  textobjects = {
-    select = {
-      enable = true,
-
-      -- Automatically jump forward to textobj, similar to targets.vim
-      lookahead = true,
-
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ["af"] = "@function.outer",
-        ["if"] = "@function.inner",
-        ["ac"] = "@class.outer",
-        ["ic"] = "@class.inner",
-      },
-      -- You can choose the select mode (default is charwise 'v')
-      selection_modes = {
-        ["@parameter.outer"] = "v", -- charwise
-        ["@function.outer"] = "V", -- linewise
-        ["@class.outer"] = "<c-v>", -- blockwise
-      },
-      -- If you set this to `true` (default is `false`) then any textobject is
-      -- extended to include preceding xor succeeding whitespace. Succeeding
-      -- whitespace has priority in order to act similarly to eg the built-in
-      -- `ap`.
-      include_surrounding_whitespace = true,
-    },
-    swap = {
-      enable = true,
-      swap_next = {
-        ["],"] = "@parameter.inner",
-      },
-      swap_previous = {
-        ["[,"] = "@parameter.inner",
-      },
-    },
-    move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = {
-        ["]m"] = "@function.outer",
-        ["]]"] = "@class.outer",
-      },
-      goto_next_end = {
-        ["]M"] = "@function.outer",
-        ["]["] = "@class.outer",
-      },
-      goto_previous_start = {
-        ["[m"] = "@function.outer",
-        ["[["] = "@class.outer",
-      },
-      goto_previous_end = {
-        ["[M"] = "@function.outer",
-        ["[]"] = "@class.outer",
-      },
     },
   },
 })
