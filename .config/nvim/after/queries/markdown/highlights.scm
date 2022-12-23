6dbe54b7febe0af82a8a8ae2d8566c501a7af1da
Phelipe Teles <phelipe_teles@hotmail.com>
2022-12-23 09:22:17 -0300

    Improve mdx treesitter syntax highlighting

--- .config/nvim/after/queries/markdown/highlights.scm
+++ .config/nvim/after/queries/markdown/highlights.scm
@@ -3,3 +3,5 @@
 ((inline) @_inline (#match? @_inline "^\(import\|export\)")) @nospell
 
 ((html_block) @_block (#match? @_block "\s*[<]/\?[A-Z]")) @nospell
+
+((paragraph) @_block (#match? @_block "\s*[<]/\?[A-Z]")) @nospell
--- .config/nvim/after/queries/markdown/injections.scm
+++ .config/nvim/after/queries/markdown/injections.scm
@@ -3,3 +3,5 @@
 ((inline) @_inline (#match? @_inline "^\(import\|export\)")) @tsx
 
 ((html_block) @_block (#match? @_block "\s*[<]/\?[A-Z]")) @tsx
+
+((paragraph) @_block (#match? @_block "\s*[<]/\?[A-Z]")) @tsx
