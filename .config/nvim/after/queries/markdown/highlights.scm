9aca8b73095e64d2d9ba1c40abed6aaba5c98e24
Phelipe Teles <phelipe_teles@hotmail.com>
2023-01-22 20:34:30 -0300

    Remove redundant queries

--- .config/nvim/after/queries/markdown/highlights.scm
+++ .config/nvim/after/queries/markdown/highlights.scm
@@ -1,7 +1,3 @@
 ; extends
 
 ((inline) @_inline (#match? @_inline "^\(import\|export\)")) @nospell
-
-((html_block) @_block (#match? @_block "\s*[<]/\?[A-Z]")) @nospell
-
-((paragraph) @_block (#match? @_block "\s*[<]/\?[A-Z]")) @nospell
--- .config/nvim/after/queries/markdown/injections.scm
+++ .config/nvim/after/queries/markdown/injections.scm
@@ -1,7 +1,3 @@
 ; extends
 
 ((inline) @_inline (#match? @_inline "^\(import\|export\)")) @tsx
-
-((html_block) @_block (#match? @_block "\s*[<]/\?[A-Z]")) @tsx
-
-((paragraph) @_block (#match? @_block "\s*[<]/\?[A-Z]")) @tsx
