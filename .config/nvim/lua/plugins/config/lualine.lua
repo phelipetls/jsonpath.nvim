3e6b2a77daa18c55298f748b1e37188ac8c61f3d
Phelipe Teles <phelipe_teles@hotmail.com>
2023-04-21 16:34:53 -0300

    Simplify statusline

--- .config/nvim/lua/plugins/config/lualine.lua
+++ .config/nvim/lua/plugins/config/lualine.lua
@@ -14,24 +14,6 @@ local function fugitivestatusline()
   return string.format("[Git:(%s)]", truncated_branch)
 end
 
-local function filename()
-  local bufname = vim.api.nvim_buf_get_name(0)
-
-  if not bufname or bufname == "" then
-    return "[No Name]"
-  end
-
-  return vim.fn.fnamemodify(bufname, ":.")
-end
-
-local function modified()
-  return "[+]"
-end
-
-local function readonly()
-  return "[-]"
-end
-
 require("lualine").setup({
   options = {
     icons_enabled = false,
@@ -40,45 +22,23 @@ require("lualine").setup({
   },
   sections = {
     lualine_a = {
-      {
-        "mode",
-      },
+      "mode",
     },
     lualine_b = {
-      {
-        fugitivestatusline,
-      },
+      fugitivestatusline,
     },
     lualine_c = {
       {
-        filename,
+        "%f",
+        type = "stl",
       },
       {
-        modified,
-        cond = function()
-          return vim.bo.modified
-        end,
+        "%m",
+        type = "stl",
         padding = { left = 0, right = 1 },
       },
-      {
-        readonly,
-        padding = { left = 0, right = 1 },
-        cond = function()
-          return not vim.bo.modifiable or vim.bo.readonly
-        end,
-      },
     },
-    lualine_x = {
-      {
-        "diagnostics",
-      },
-      {
-        "fileformat",
-      },
-      {
-        "filetype",
-      },
-    },
+    lualine_x = {},
     lualine_y = {},
     lualine_z = {
       {
@@ -89,35 +49,20 @@ require("lualine").setup({
   },
   inactive_sections = {
     lualine_a = {},
-    lualine_b = {
-      {
-        fugitivestatusline,
-      },
-    },
+    lualine_b = {},
     lualine_c = {
       {
-        filename,
+        "%f",
+        type = "stl",
       },
       {
-        modified,
-        cond = function()
-          return vim.bo.modified
-        end,
+        "%m",
+        type = "stl",
         padding = { left = 0, right = 1 },
-      },
-      {
-        readonly,
-        padding = { left = 0, right = 1 },
-        cond = function()
-          return not vim.bo.modifiable or vim.bo.readonly
-        end,
       },
     },
     lualine_x = {},
     lualine_y = {},
-    lualine_z = {
-      "filetype",
-    },
   },
   extensions = {
     "fugitive",
@@ -149,11 +94,9 @@ require("lualine").setup({
     {
       sections = {
         lualine_a = {
-          {
-            function()
-              return string.format("%d args", #vim.fn.argv())
-            end,
-          },
+          function()
+            return string.format("%d args", #vim.fn.argv())
+          end,
         },
         lualine_b = {},
         lualine_c = {},
