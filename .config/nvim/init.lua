bb41e5536030688579799bcc0231900374706fe9
Phelipe Teles <phelipe_teles@hotmail.com>
2023-05-25 11:02:52 -0300

    Don't adapt Twiggy buffer to branch names length

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -60,7 +60,7 @@ vim.cmd("packadd! vim-fugitive-blame-ext")
 vim.cmd("packadd! vim-twiggy")
 vim.g.twiggy_group_locals_by_slash = 0
 vim.g.twiggy_local_branch_sort = "mru"
-vim.g.twiggy_adapt_columns = 1
+vim.g.twiggy_adapt_columns = 0
 vim.g.twiggy_show_full_ui = 0
 
 -- file navigation
