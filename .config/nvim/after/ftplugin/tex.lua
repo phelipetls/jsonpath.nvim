06d1da731a46f961c76bb1a63866e9f37a2156da
Phelipe Teles <phelipe_teles@hotmail.com>
2023-07-12 15:12:41 -0300

    Remove Neovim configs I rarely use

--- .config/nvim/after/ftplugin/tex.lua
+++ .config/nvim/after/ftplugin/tex.lua
@@ -1,4 +1,3 @@
 vim.wo.spell = true
 vim.bo.spelllang = "pt,en_us"
-vim.bo.textwidth = 80
 vim.bo.formatlistpat = [[^\\\s*\\\\item\\s*]]
--- .config/nvim/after/ftplugin/typescript.lua
+++ .config/nvim/after/ftplugin/typescript.lua
@@ -1,3 +1,1 @@
 vim.cmd("runtime! ftplugin/javascript.lua")
-
-vim.keymap.set("n", "<F5>", "<cmd>w !deno<CR>", { buffer = true, noremap = true })
