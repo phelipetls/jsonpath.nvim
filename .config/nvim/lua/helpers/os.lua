89bfeef65b05de65da9cc304362d360c567e5347
Phelipe Teles <phelipe_teles@hotmail.com>
2023-07-16 17:53:23 -0300

    Fix gx function to open file under cursor

--- .config/nvim/lua/helpers/os.lua
+++ .config/nvim/lua/helpers/os.lua
@@ -2,7 +2,7 @@ local M = {}
 
 local function show_error(error_message)
   vim.cmd.echohl("ErrorMsg")
-  vim.cmd.echo(error_message)
+  vim.cmd.echo(string.format("'%s'", error_message))
   vim.cmd.echohl("None")
 end
 
@@ -28,18 +28,25 @@ end
 M.open = function(fname)
   local open_command = get_open_command()
 
-  if vim.fn.executable(open_command) == 0 then
+  if vim.fn.executable(open_command) ~= 1 then
     show_error(string.format("The program %s is not executable", open_command))
     return
   end
 
-  vim.fn.jobstart(string.format("%s %s", open_command, vim.fn.shellescape(fname), {
+  local cmd = string.format("%s %s", open_command, vim.fn.shellescape(fname))
+
+  vim.fn.jobstart(cmd, {
     on_exit = function(_, status)
       if status > 0 then
+        if vim.fn.filereadable(fname) ~= 1 or vim.fn.isdirectory(fname) ~= 1 then
+          show_error(string.format("Could not open %s because it does not exist", fname))
+          return
+        end
+
         show_error(string.format("Failed to open %s with %s", fname, open_command))
       end
     end,
-  }))
+  })
 end
 
 local function is_normal_mode()
