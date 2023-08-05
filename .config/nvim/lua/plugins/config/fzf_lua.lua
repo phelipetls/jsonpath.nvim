09b526fac949ff359dd0b13e1e85f4d6a307a39d
Phelipe Teles <phelipe_teles@hotmail.com>
2023-08-05 12:00:06 -0300

    Add mapping and commands for live_grep with fzf-lua

--- .config/nvim/lua/plugins/config/fzf_lua.lua
+++ .config/nvim/lua/plugins/config/fzf_lua.lua
@@ -14,3 +14,6 @@ vim.keymap.set("n", "<space>r", "<cmd>FzfLua oldfiles<CR>")
 vim.keymap.set("n", "<space>p", "<cmd>FzfLua commands<CR>")
 vim.keymap.set("n", "<space>gb", "<cmd>FzfLua git_branches<CR>")
 vim.keymap.set("n", "<space>gs", "<cmd>FzfLua git_stash<CR>")
+
+vim.keymap.set("n", "<space>gr", "<cmd>FzfLua live_grep_native<CR>")
+vim.api.nvim_create_user_command("Rg", "FzfLua live_grep_native", {})
