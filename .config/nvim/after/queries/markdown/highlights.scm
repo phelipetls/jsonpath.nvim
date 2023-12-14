ed148f6231bcb4b87455579771c49f3f33a0ee34
Phelipe Teles <phelipe_teles@hotmail.com>
2023-12-14 14:51:24 -0300

    Update treesitter queries for MDX

--- .config/nvim/after/queries/markdown/highlights.scm
+++ .config/nvim/after/queries/markdown/highlights.scm
@@ -1,3 +1,4 @@
 ; extends
 
-((inline) @_inline (#match? @_inline "^\(import\|export\)")) @nospell
+((inline) @_inline (#lua-match? @_inline "^%s*import")) @nospell
+((inline) @_inline (#lua-match? @_inline "^%s*export")) @nospell
--- .config/nvim/after/queries/markdown/injections.scm
+++ .config/nvim/after/queries/markdown/injections.scm
@@ -1,3 +1,8 @@
 ; extends
 
-((inline) @_inline (#match? @_inline "^\(import\|export\)")) @tsx
+((inline) @injection.content
+  (#lua-match? @injection.content "^%s*import")
+  (#set! injection.language "typescript"))
+((inline) @injection.content
+  (#lua-match? @injection.content "^%s*export")
+  (#set! injection.language "typescript"))
