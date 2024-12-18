50d216e9df2e3a5d047fe031a9bfe668a84b4fc2
Phelipe Teles <phelipe_teles@hotmail.com>
2024-12-18 18:44:21 -0300

    Add GitHub copilot Neovim plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -133,6 +133,14 @@ vim.g["test#custom_transformations"] = {
   end,
 }
 
+-- ai
+vim.cmd("packadd! copilot.vim")
+vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+  expr = true,
+  replace_keycodes = false
+})
+vim.g.copilot_no_tab_map = true
+
 -- }}}
 -- {{{ settings
 
